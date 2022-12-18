3952358b6ea7461f86ecc44037a0550ee11950af
yxrellx <yarella.rojas02@inacapmail.cl>
2022-12-17 21:11:28 -0300

    avance

--- app/src/main/java/com/example/biblioteca/Principal1.java
+++ app/src/main/java/com/example/biblioteca/Principal1.java
@@ -1,8 +1,13 @@
 package com.example.biblioteca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayout.OnTabSelectedListener;
@@ -13,13 +18,25 @@ public class Principal1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal1);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
     }
 
-    //TabLayout tl = (TabLayout) findViewById(R.id.tablayout);
-    //tl.addOnTabSelected(new TabLayout.OnTabSelectedListener() {
-       // @Override
-       // public void onTabSelected(TabLayout.Tab Tab){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
 
-        //}
-   // }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id==R.id.op1){
+            intent i = new Intent(this,)
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
